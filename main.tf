2a69d4a5616c5b3d95327bf1e07485fc3e2beb7b
Vigneswaran-Murthy <158814881+Vigneswaran-Murthy@users.noreply.github.com>
2024-12-28 22:43:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ data "aws_ami" "latest-rhel9-image" {
 }
 
 resource "aws_instance" "my_server" {
-        count                      =3
+        count                      =1
     ami                         = data.aws_ami.latest-rhel9-image.id
     instance_type               = var.instance_type
     subnet_id                   = aws_subnet.dev-subnet-1.id
